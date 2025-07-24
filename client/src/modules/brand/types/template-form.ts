75e97896369869e6b2c34ad5bfe5c5382643d6d0
Angelo García <angelogarcia@mailes.ueb.edu.ec>
2025-07-24 13:43:14 -0500

    style(brand): change format export to lowerCase a UpperCase

--- client/src/modules/brand/types/template-form.ts
+++ client/src/modules/brand/types/template-form.ts
@@ -1,16 +1,16 @@
 import { z } from 'zod'
 import { I_Brand } from '@/modules/brand/types/brand'
 
-export const brandSchema = z.object({
+export const BrandSchema = z.object({
 	name: z.string().min(3, 'Mínimo 3 caracteres').max(50, 'Máximo 50 caracteres'),
 	description: z.string().max(300, 'Máximo 300 caracteres').optional().or(z.literal('')),
 })
 
-export type brandFormData = z.infer<typeof brandSchema>
+export type BrandFormData = z.infer<typeof BrandSchema>
 
 export interface TemplateFormProps {
 	isOpen: boolean
 	currentbrand?: I_Brand
 	onClose: () => void
-	onSubmit: (data: brandFormData) => Promise<void>
+	onSubmit: (data: BrandFormData) => Promise<void>
 }
