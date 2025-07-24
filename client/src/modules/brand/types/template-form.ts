af87df9c41c37965792e9feaf65a0675524ec9c7
Angelo García <angelogarcia@mailes.ueb.edu.ec>
2025-07-24 13:08:32 -0500

    fix(): shcheme valite fields leghtn

--- client/src/modules/brand/types/template-form.ts
+++ client/src/modules/brand/types/template-form.ts
@@ -2,15 +2,8 @@ import { z } from 'zod'
 import { I_Brand } from '@/modules/brand/types/brand'
 
 export const brandSchema = z.object({
-	name: z
-		.string()
-		.min(3, 'El nombre debe tener al menos 3 caracteres')
-		.max(100, 'El nombre no puede exceder 100 caracteres'),
-	description: z
-		.string()
-		.max(255, 'La descripción no puede exceder 255 caracteres')
-		.optional()
-		.or(z.literal('')),
+	name: z.string().min(3, 'Mínimo 3 caracteres').max(25, 'Máximo 25 caracteres'),
+	description: z.string().max(300, 'Máximo 300 caracteres').optional().or(z.literal('')),
 })
 
 export type brandFormData = z.infer<typeof brandSchema>
