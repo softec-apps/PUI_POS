ff014670163f152ea4264d8e5cc5999aac00df94
ANHELL0S <angelogarcia@mailes.ueb.edu.ec>
2025-07-29 15:05:15 -0500

    feature: add fastify, more fast

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,20 +1,28 @@
 import 'dotenv/config'
-
 import { NestFactory } from '@nestjs/core'
 import { ConfigService } from '@nestjs/config'
-
 import { AppModule } from '@/app.module'
 import type { AllConfigType } from '@/config/config.type'
-
 import { setupSwaggerUI } from '@/config/swagger-ui.config'
 import { startApplication } from '@/config/app-startup.config'
 import { configureGlobalAppSettings } from '@/config/app-settings.config'
 import { configureSwaggerDocument } from '@/config/swagger-document.config'
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    logger: ['error', 'warn'],
-  })
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({
+      logger: false, // Ya tienes esto, pero también necesitas silenciar NestJS
+    }),
+    {
+      // Silenciar completamente los logs de NestJS
+      logger: false,
+    },
+  )
 
   // Configuración global de la aplicación
   configureGlobalAppSettings(app)
